08d76be8655631fab351881c59343deaa8652d12
sofiat-olaoebikan <sofiat@aims.edu.gh>
2015-08-29 21:01:58 +0100

    missing commas added

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(name='hopcroftkarp',
-      version='1.2.0',
+      version='1.2.0a1',
       description='Implementation of HopcroftKarp\'s algorithm',
-      long_description='{0:s}'.format(
-          open('README.rst').read()),
+      long_description='{0:s}'.format(open('README.rst').read()),
       author='Sofiat Olaosebikan',
       author_email='sofiat@aims.edu.gh',
-      url='https://github.com/sofiat-olaosebikan/hopcroftkarp'
-      license = 'GPL'
-      keywords = 'hopcroftkarp algorithm maximum cardinality matching bipartite graphs'
-      
+      url='https://github.com/sofiat-olaosebikan/hopcroftkarp',
+      license = 'GPL',
+      keywords = 'hopcroftkarp algorithm maximum cardinality matching bipartite graphs',
       packages=find_packages(),
       zip_safe=True,
       package_dir={'hopcroftkarp': 'hopcroftkarp'},
 	  test_suite='tests',
-      
       classifiers=[
           'Development Status :: 3 - Alpha',
+          'Intended Audience :: Science/Research',
           'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
           'Programming Language :: Python :: 3',
-          'Intended Audience :: Science/Research',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
           ],
       )
