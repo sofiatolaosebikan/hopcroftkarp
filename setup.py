591d6d25e884cb1c8e523682ee3f2d1f46f69c72
sofiat-olaoebikan <sofiat@aims.edu.gh>
2015-08-30 01:09:25 +0100

    version number upgraded added python 2.7

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
 from setuptools import setup, find_packages
 
 setup(name='hopcroftkarp',
-      version='1.2.0a1',
+      version='1.2.1',
       description='Implementation of HopcroftKarp\'s algorithm',
       long_description='{0:s}'.format(open('README.rst').read()),
       author='Sofiat Olaosebikan',
       author_email='sofiat@aims.edu.gh',
       url='https://github.com/sofiat-olaosebikan/hopcroftkarp',
       license = 'GPL',
-      keywords = 'hopcroftkarp algorithm maximum cardinality matching bipartite graphs',
+      keywords = 'hopcroftkarp algorithm, maximum cardinality matching, bipartite graphs',
       packages=find_packages(),
       zip_safe=True,
       package_dir={'hopcroftkarp': 'hopcroftkarp'},
-	  test_suite='tests',
+	  test_suite='test',
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Science/Research',
           'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3.2',
           'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
           ],
       )
+      
